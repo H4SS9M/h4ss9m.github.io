4322a20add2d7837357e7c29b02f353f3c1e45a8
H4SS9M🐄🇻🇪 <privatealfonso95@gmail.com>
2026-09-11 03:19:56 -0400

    Update chain_poops.js

--- chain_poops.js
+++ chain_poops.js
@@ -436,7 +436,7 @@ let allDone = false;
         // NETCONTROL: a1=0, a4=4, eventos int64
         function netevent(sock, event) {
             argDv.setUint32(0, sock >>> 0, true);
-            const r = sc(SYS.netcontrol, -1, event, argAddr, 4).i32;
+            const dummy = sc(SYS.socket, 2 /* AF_INET */, SOCK_STREAM, 0).i32;
             return { rv: r, err: r === -1 ? errno() : 0 };
         }
 
