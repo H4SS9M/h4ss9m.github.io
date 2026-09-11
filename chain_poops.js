c02a8120c84ec70d24ceccdd13b4ff871b895f95
H4SS9M🐄🇻🇪 <privatealfonso95@gmail.com>
2026-09-11 03:16:15 -0400

    Update chain_poops.js

--- chain_poops.js
+++ chain_poops.js
@@ -436,7 +436,7 @@ let allDone = false;
         // NETCONTROL: a1=0, a4=4, eventos int64
         function netevent(sock, event) {
             argDv.setUint32(0, sock >>> 0, true);
-            const r = sc(SYS.netcontrol, 0, event, argAddr, 8).i32;
+            const r = sc(SYS.netcontrol, -1, event, argAddr, 4).i32;
             return { rv: r, err: r === -1 ? errno() : 0 };
         }
 
